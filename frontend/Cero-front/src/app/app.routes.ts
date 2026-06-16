20a60edb54ac92bcce73f0e6dda2985400dfccf9
Kallik <h.jarqueg@gmail.com>
2026-06-16 11:37:30 +0200

    [main]: add home, login, register and 404

--- frontend/Cero-front/src/app/app.routes.ts
+++ frontend/Cero-front/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
