4fc44115ffbc5d2c1e6cf17dd7f8f56a0f815738
Kallik <h.jarqueg@gmail.com>
2026-06-16 12:25:05 +0200

    [main]: add routes

--- frontend/Cero-front/src/app/app.routes.ts
+++ frontend/Cero-front/src/app/app.routes.ts
@@ -3,6 +3,16 @@ import { HomeComponent } from './features/home/home.component';
 import { LoginComponent } from './features/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
 import { NotFoundComponent } from './features/not-found/not-found.component';
+import { PortfolioComponent } from './features/portfolio/portfolio.component';
+import {
+  InformativaComponent
+} from './features/informativa/informativa.component';
+import { EcommerceComponent } from './features/ecommerce/ecommerce.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { BlogComponent } from './features/blog/blog.component';
+import { LandingComponent } from './features/landing/landing.component';
+import { SaasComponent } from './features/saas/saas.component';
+import { ReservasComponent } from './features/reservas/reservas.component';
 
 
 export const routes: Routes = [
@@ -19,7 +29,39 @@ export const routes: Routes = [
     component: RegisterComponent
   },
   {
+    path: 'portfolio',
+    component: PortfolioComponent
+  },
+  {
+    path: 'informativa',
+    component: InformativaComponent
+  },
+  {
+    path: 'ecommerce',
+    component: EcommerceComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  },
+  {
+    path: 'landing',
+    component: LandingComponent
+  },
+  {
+    path: 'reservas',
+    component: ReservasComponent
+  },
+  {
+    path: 'saas',
+    component: SaasComponent
+  },
+  {
     path: '**',
     component: NotFoundComponent
-  }
+  },
 ];
